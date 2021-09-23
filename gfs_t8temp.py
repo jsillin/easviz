16bcae46c20d3b754ea4ff012e563c4f8b4ed33b
jsillin <jack.sillin@gmail.com>
2021-09-22 23:41:06 -0400

    Update gfs_t8temp.py
    add cyclic temp, don't open t8 file twice, higher res output

--- gfs_t8temp.py
+++ gfs_t8temp.py
@@ -21,6 +21,7 @@ import matplotlib.colors as col
 from metpy.plots import ctables
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, Normalize
 import supplementary_tools as spt 
+from cartopy.util import add_cyclic_point 
 
 def load_basemap(map_path):
     img=plt.imread(map_path)
@@ -45,7 +46,7 @@ newnorm = Normalize(-44,48)
 
 def anim_t8():
     plt.ioff()
-    f = plt.figure(figsize = (8, 4), dpi = 200)
+    f = plt.figure(figsize = (8, 4), dpi = 200, tight_layout=True)
     f.clf()
 
     # Create plot that can handle geographical data
@@ -57,8 +58,6 @@ def anim_t8():
 
     fname = '/home/jhs389/plotting/gfs.t'+runtime+'z.pgrb2.0p25.f000'
 	
-    ds = xr.open_dataset(fname,engine='cfgrib',filter_by_keys={'typeOfLevel': 'isobaricInhPa'})
-    t8 = ds['t'].sel(isobaricInhPa=850)-273.15	
     
     frames = range(0,120,1)
     def anim(i):
@@ -76,23 +75,23 @@ def anim_t8():
         fname = '/home/jhs389/plotting/gfs.t'+runtime+'z.pgrb2.0p25.f'+pfhr
         ds = xr.open_dataset(fname,engine='cfgrib',filter_by_keys={'typeOfLevel': 'isobaricInhPa'})
         t8 = ds['t'].sel(isobaricInhPa=850)-273.15	
-
+        t8 = add_cyclic_point(t8,coord=t8.lon)
         dtfs = pd.to_datetime(t8.time.values+t8.step.values).strftime('%Y-%m-%d_%H%MZ') 
         ax.set_extent((-157,0,0,90))
 
         ax.contourf(t8.longitude,t8.latitude,t8,norm=newnorm,cmap=newcmap,antialiased=True,levels=range(-40,40,1),alpha=0.7,transform=ccrs.PlateCarree(),add_colorbar=True)
         ax.contour(t8.longitude,t8.latitude,t8,levels=[0],colors=['blue'],transform=ccrs.PlateCarree())
 
-        ax.set_title('850hPa Temperature (C)',fontsize=16)
-        ax.set_title('Valid: '+dtfs,loc='right',fontsize=11)
-        ax.set_title('GFS Forecast via NOAA \nPlot by Jack Sillin',loc='left',fontsize=11)
+        ax.set_title('850hPa Temperature (C)',fontsize=11)
+        ax.set_title('Valid: '+dtfs,loc='right',fontsize=8)
+        ax.set_title('GFS Forecast via NOAA \nPlot by Jack Sillin',loc='left',fontsize=8)
         
         plt.ion()
         plt.draw()
 
     anim = manim.FuncAnimation(f, anim,frames, repeat=False)
         
-    anim.save('t8_anim_test1.mp4', fps=12, codec='h264', dpi=120)
+    anim.save('t8_anim_test2.mp4', fps=12, codec='h264', dpi=150)
     plt.ion()
 
 anim_t8()
